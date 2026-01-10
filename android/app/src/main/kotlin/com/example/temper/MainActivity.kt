e15a299fffff51d056517eb4f2404459830122ea
AAdithya <aadithyakrishna006@gmail.com>
2026-01-10 22:46:02 +0530

    pushed onto wrong branch

--- android/app/src/main/kotlin/com/example/temper/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.temper
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- lib/auth/domain/entities/user_entity.dart
+++
@@ -1,9 +0,0 @@
-class UserEntity {
-  final String username;
-  final String role; // We will expect values like 'admin' or 'guest'
-
-  UserEntity({
-    required this.username,
-    required this.role,
-  });
-}
